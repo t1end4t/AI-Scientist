3c98ef9ba41aec62d61e639c401da9f4c3968cb4
t1end4t <tiendatarse3004@gmail.com>
2025-06-22 17:32:53 +0700

    update module

--- run_scientist.sh
+++ run_scientist.sh
@@ -8,12 +8,12 @@ fi
 
 # Run the Python script with the desired arguments
 python3 launch_scientist_step1.py \
-  --experiment "nanoGPT" \
+  --experiment "" \
   --model "gemini-2.0-flash" \
   --writeup "latex" \
   --parallel 1 \
   --improvement \
   --gpus "0" \
-  --num-ideas 5 \
+  --num-ideas 1 \
   --engine "openalex"
 
